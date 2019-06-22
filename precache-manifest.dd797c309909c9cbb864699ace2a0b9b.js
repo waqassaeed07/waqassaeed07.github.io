8b03c6db1259af621b1daa0e05fc2139c5182916
waqassaeed07 <waqas.saeed007@gmail.com>
2019-06-22 10:07:39 +0500

    webs

---
+++ precache-manifest.dd797c309909c9cbb864699ace2a0b9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22448dc72753d23fc57a2a551891c986",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc901d7cef8d50a30d25",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "50dffc2e5f7f672f6ce6",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "bc901d7cef8d50a30d25",
+    "url": "/static/js/2.127a4177.chunk.js"
+  },
+  {
+    "revision": "50dffc2e5f7f672f6ce6",
+    "url": "/static/js/main.235f54c4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "094105d5157c311e799ff109a9fd503b",
+    "url": "/static/media/logo.094105d5.png"
+  }
+]);
